40ea7b31f2721370c4c2b25e2922173d42984a85
neil <neil.1475369@gmail.com>
2018-09-21 00:35:22 +0100

    vue working from offline json in test build

--- app/appcode.js
+++ app/appcode.js
@@ -1,6 +1,11 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+        results: []
+      },
+      mounted() {
+        // axios.get("https://randomapi.com/api/7uhi29ps?key=L65B-46NZ-LQF5-BP1G&results=20&page=4")
+        axios.get("../lib/offline-json.json")
+        .then(response => {this.results = response.data.results})
     }
-  })
+})
