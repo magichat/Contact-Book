cab1dac0d08213612753012457f4e0c94983489c
neil <neil.1475369@gmail.com>
2018-09-21 09:14:22 +0100

    added popup logic

--- app/appcode.js
+++ app/appcode.js
@@ -1,7 +1,17 @@
 var app = new Vue({
     el: '#app',
     data: {
-        results: []
+        results: [],
+        fullData: []
+      },
+      methods: {
+        fullDataSet: function (targetDataSet) {
+            this.fullData = targetDataSet;
+            document.getElementsByClassName("popup")[0].classList.remove("popup__hide");
+        },
+        hidePopup: function () {
+            document.getElementsByClassName("popup")[0].classList.add("popup__hide");
+        }
       },
       mounted() {
         // axios.get("https://randomapi.com/api/7uhi29ps?key=L65B-46NZ-LQF5-BP1G&results=20&page=4")
